be7a725fa4006267935ea89dcd4dcb68ef8e5aa9
dave0xE7 <david.wieser.202@gmail.com>
2020-08-22 05:39:55 +0200

    dsf

--- scripts/vms.sh
+++ scripts/vms.sh
@@ -2,3 +2,6 @@
 echo "installing vms"
 
 sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin -y
+#sudo apt install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager -y
+
+systemctl status libvirtd.service
