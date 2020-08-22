054917c27a38b558653b120bab8b7f043f4add6c
dave0xE7 <david.wieser.202@gmail.com>
2020-08-22 05:47:51 +0200

    webserver

--- scripts/webspaces.sh
+++ scripts/webspaces.sh
@@ -1,2 +1,22 @@
 
 echo "installing webspaces"
+
+apt update -y
+apt install sudo curl git gnupg unzip -y
+apt install php php-gd php-mbstring php-xml -y
+
+apt install apache2 libapache2-mod-php -y
+
+
+cd /tmp
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
+dpkg -i mysql-apt-config*
+apt update -y
+
+apt install mysql-server php-mysql mysql-client -y
+apt install php-mysql -y
+
+systemctl restart mysql
+systemctl restart apache2 
+
+echo "webserver finished"
