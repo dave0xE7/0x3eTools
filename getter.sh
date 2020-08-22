a06ddcfb48acd15d80dca5fb2d3d28e77d8b66f7
dave0xE7 <service@localhost.localdomain>
2020-08-22 03:57:39 +0200

    done

--- getter.sh
+++ getter.sh
@@ -2,9 +2,17 @@
 
 echo "getting 0x3e tools onto this machine..."
 
+path=~/.0x3e
+echo $path
 
+if test -d $path; then
+    mkdir -p $path;
+fi
+echo $0
+#cp $0 $path/.
+cd $path
 
-
+git clone https://github.com/dave0xE7/0x3eTools .
 
 echo "done"
 
